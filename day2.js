5178c1c1188bfc1c65c8f68c7bc7a217680dcc75
jork41989 <jork41989@gmail.com>
2020-12-02 11:39:52 -0500

    day2 part 2

--- day2.js
+++ day2.js
@@ -12,7 +12,6 @@ let passValid = function (arr){
     let letter = splt[1][0]
     let password = splt[2]
     let letCount = password.split(letter).length - 1;
-    console.log(high, low, count);
     if (letCount <= high && letCount >= low){
       count++
     }
@@ -21,6 +20,27 @@ let passValid = function (arr){
 
 }
 
+let passValidDuex = function (arr){
+  let count = 0;
+  arr.forEach((pass) => {
+    let splt = pass.split(" ");
+    let rangeSplt = splt[0].split("-");
+    let highIdx = rangeSplt[1];
+    highIdx = parseInt(highIdx) - 1
+    let lowIdx = rangeSplt[0];
+    lowIdx = parseInt(lowIdx) - 1;
+    let letter = splt[1][0];
+    let password = splt[2];
+    if (password[lowIdx] === letter && password[highIdx] != letter) {
+        count++;
+    } else if (password[highIdx] === letter && password[lowIdx] != letter) {
+      count++;
+    }
+  });
+  return count;
+}
+
 let testArr = ["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"];
 
 console.log(passValid(passArr))
+console.log(passValidDuex(passArr))
